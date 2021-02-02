48dc752a6aa6b0eb3f7a47056336fdd692e209b1
M0rdhau <dachimshvidobadze@gmail.com>
2021-02-02 04:28:48 +0200

    added a bekeko

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import postService from '../services/posts'
 import { useSelector } from 'react-redux'
+import { useField } from '../hooks'
+import marked from 'marked'
 
 const PostForm = () => {
   const userInfo = useSelector(state => state.user)
 
+  const content = useField('content')
+  const title = useField('title')
+  let author = ''
+  if(userInfo.username) { author = userInfo.username }
+
   const publishPost = (event) => {
     event.preventDefault()
-    const content = event.target.posttext.value
-    const title = event.target.title.value
-    const author = userInfo.username
+    const contentValue = content.value
+    const titleValue = title.value
+    // const author = userInfo.username
     const userToken = userInfo.token
-    event.target.title.value = ''
-    event.target.posttext.value = ''
+    content.clear()
+    title.clear()
     const postToCreate = {
-      content,
-      title,
+      content: contentValue,
+      title: titleValue,
       author
     }
     postService.setToken(userToken)
@@ -25,10 +32,15 @@ const PostForm = () => {
   return (
     <div>
       <form className="postForm" onSubmit={publishPost} >
-        <input name='title' />
-        <textarea name='posttext' />
+        <input name='title' onChange={title.onChange}/>
+        <textarea name='posttext' onChange={content.onChange}/>
         <button type='submit'> Publish! </button>
       </form>
+      <div className="postTitles">
+        <h2>{title.value}</h2>
+        <h3><em>By: {author}</em></h3>
+      </div>
+      <div dangerouslySetInnerHTML={ { __html: marked(content.value) } } ></div>
     </div>
   )
 }
